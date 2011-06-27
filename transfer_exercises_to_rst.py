99e53e1bf2370df0854afe08bafd12fb2d1861c9
Nolan Brubaker <nbrubake@nd.edu>
2011-06-27 14:14:47 -0400

    Added a script for tranferring solution scripts into rst files.

---
+++ transfer_exercises_to_rst.py
@@ -0,0 +1,27 @@
+"""
+Takes all the exercise scripts in the current directory and converts them into a format suitable for reStructured Text inline examples.
+
+Useful if you want to have executable code that also gets included into a document.
+"""
+from glob import glob
+
+exercise_scripts = glob("exercise*.py")
+
+for script in exercise_scripts:
+    ex_number = script[-4]
+
+    with open(script, "r") as f:
+        c = f.read()
+
+    solution_text = ["Solution %s::" % ex_number]
+    c = c.split("\n")
+
+    # Append indentation and rejoin.
+    c = solution_text + ["   " + l for l in c]
+    c = "\n".join(c)
+
+    with open("solutions/solution%s.rst" % ex_number, "w") as f:
+        f.write(c)
+
+
+
